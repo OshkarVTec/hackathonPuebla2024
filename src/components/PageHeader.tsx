e10e373495c44d6fd775fd17bc95ffde1b73398f
OshkarVTec <a01275287@tec.mx>
2024-06-19 00:39:01 -0600

    add dots

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,9 +1,11 @@
 import BackButton from "./BackButton";
+import { BsThreeDots } from "react-icons/bs";
 
 export default function PageHeader() {
 	return (
-		<div className="m-4">
+		<div className="m-4 flex justify-between items-center">
 			<BackButton />
+			<BsThreeDots size={30} color="#9c9c9c" />
 		</div>
 	);
 }
