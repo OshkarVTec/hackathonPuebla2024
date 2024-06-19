9a3c8a41e19812175d7a8028cb403af831921475
OshkarVTec <a01275287@tec.mx>
2024-06-19 02:02:36 -0600

    fix white space

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,13 +8,13 @@ export default function Home() {
 	return (
 		<div className="bg-[#6D57FC] h-screen flex flex-col ">
 			<img src={homeImg} className="grow-0" />
-			<div className="bg-white rounded-t-3xl h-full">
+			<div className="bg-white rounded-t-3xl h-full flex flex-col">
 				<p className="text-3xl p-4 ">
 					Comencemos a <span className="text-[#6D57FC]">desarrollar</span>{" "}
 					nuestras habilidades
 				</p>
 
-				<div className="flex flex-col justify-center">
+				<div className="flex flex-col justify-center flex-grow">
 					<div className="flex items-center p-4 gap-4">
 						<img src={learningImg} />
 						<p className="text-3xl">Aprendizaje</p>
@@ -25,7 +25,7 @@ export default function Home() {
 					</Link>
 					<Link to="/dictado" className="flex items-center p-4 gap-4">
 						<img src={recordImg} />
-						<p className="text-3xl">Tomar clase</p>
+						<p className="text-3xl">Tomar</p>
 					</Link>
 				</div>
 			</div>
